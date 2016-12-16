ed76f92dd6470ed59dde0e68c4b54aa323531d83
Ezra Sandzer-Bell <equilibriumart@gmail.com>
2016-12-15 23:38:00 -0800

    successfully retrieve input from both brand and store forms and append all results on index page

--- app.rb
+++ app.rb
@@ -5,5 +5,21 @@ require('pry')
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get('/') do
+  @stores = Store.all
+  @brands = Brand.all
+  erb(:index)
+end
+
+post('/stores') do
+  Store.create({:name => params.fetch("store_name")})
+  @stores = Store.all
+  @brands = Brand.all
+  erb(:index)
+end
+
+post('/brands') do
+  Brand.create({:name => params.fetch("brand_name")})
+  @stores = Store.all
+  @brands = Brand.all
   erb(:index)
 end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -4,7 +4,24 @@ describe('navigation') do
   describe('routing to index', {:type => :feature}) do
     it('successfully loads index') do
       visit('/')
-      expect(page).to have_content("Hello World")
+      expect(page).to have_content("Stores and Brands")
+    end
+  end
+end
+
+describe('creating instances of brand and store class') do
+  describe('submitting forms', {:type => :feature}) do
+    it('fills out brand name form and submits') do
+      visit('/')
+      fill_in("brand_name", :with => "Nike")
+      click_button("Register New Brand")
+      expect(page).to have_content("Nike")
+    end
+    it('fills out store name form and submits') do
+      visit('/')
+      fill_in("store_name", :with => "Foot Locker")
+      click_button("Register New Store")
+      expect(page).to have_content("Foot Locker")
     end
   end
 end
