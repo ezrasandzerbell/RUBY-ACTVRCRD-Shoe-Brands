b93fcda1eb3df4f943394232766655f14716b59d
Ezra Sandzer-Bell <equilibriumart@gmail.com>
2016-12-15 22:55:53 -0800

    pass rspec for Brand object attribute (name)

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -1,1 +1,10 @@
 require('spec_helper')
+
+describe(Brand) do
+  describe('test Brand object') do
+    it('successfully loads index') do
+      brand_test = Brand.create({:name => "Nike"})
+      expect(brand_test.name).to(eq("Nike"))
+    end
+  end
+end
