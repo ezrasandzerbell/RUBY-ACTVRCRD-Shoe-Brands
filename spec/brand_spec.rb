d04ace735b200386a422504e48e179f605a2815c
Ezra Sandzer-Bell <equilibriumart@gmail.com>
2016-12-15 22:58:50 -0800

    pass rspec for brand's store id

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -2,9 +2,13 @@ require('spec_helper')
 
 describe(Brand) do
   describe('test Brand object') do
-    it('successfully loads index') do
+    it('test for name') do
       brand_test = Brand.create({:name => "Nike"})
       expect(brand_test.name).to(eq("Nike"))
+    end
+    it('successfully loads index') do
+      brand_test = Brand.create({:name => "Foot Locker", :store_id => "1"})
+      expect(brand_test.store_id).to(eq(1))
     end
   end
 end
